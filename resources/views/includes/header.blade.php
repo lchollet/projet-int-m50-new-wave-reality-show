9008cdb71f27db53b85fd4e46bd4e14282375113
Jocic Sacha <jocicsacha@mbp-de-jocic.einet.ad.eivd.ch>
2024-04-15 20:40:03 +0200

    modification header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,60 +1,44 @@
 <div class="navbar">
     <div class="navbar-inner">
         <ul class="nav">
-            <button type="button" class="btn btn-warning btn-login" href="login">Connexion</button>
-            <img src="{{ asset('storage/images/logo.png') }}" alt="logo new wave">
+            <button type="button" class="btn btn-warning btn-back"><</button>
+            {{-- <img src="{{ asset('storage/images/logo.png') }}" alt="logo new wave"> --}}
+            <div class="profile-info">
+               <div classe="bonhomme">👤</div> Bobar Bonin
+            </div>
         </ul>
     </div>
 </div>
 
 <style>
-:root {
-  --pink-1: rgba(255, 24, 163, 1);
-  --pink-2: rgba(248, 77, 174, 1);
-  --pink-3: rgba(254, 94, 158, 1);
-  --orange-1: rgba(255, 104, 35, 1);
-  --orange-2: rgba(255, 131, 72, 1);
-  --orange-3: rgba(253, 173, 102, 1);
-  --black: rgba(30, 30, 30, 1);
-  --dark-grey: rgba(79, 79, 79, 1);
-  --grey: rgba(183, 183, 183, 1);
-  --light-grey: rgba(227, 227, 227, 1);
-  --white: rgba(253, 253, 253, 1);
-  --h1-font-family: "Inter", Helvetica;
-  --h1-font-weight: 700;
-  --h1-font-size: 24px;
-  --h1-letter-spacing: 0px;
-  --h1-line-height: normal;
-  --h1-font-style: normal;
-  --h2-font-family: "Inter", Helvetica;
-  --h2-font-weight: 700;
-  --h2-font-size: 22px;
-  --h2-letter-spacing: 0px;
-  --h2-line-height: normal;
-  --h2-font-style: normal;
-  --h3-font-family: "Inter", Helvetica;
-  --h3-font-weight: 700;
-  --h3-font-size: 20px;
-  --h3-letter-spacing: 0px;
-  --h3-line-height: normal;
-  --h3-font-style: normal;
-  --paragraphe-font-family: "Inter", Helvetica;
-  --paragraphe-font-weight: 400;
-  --paragraphe-font-size: 20px;
-  --paragraphe-letter-spacing: 0px;
-  --paragraphe-line-height: normal;
-  --paragraphe-font-style: normal;
-}
+
     img {
         width: 10%;
         max-width: 1000px;
     }
 
+    .bonhomme {
+    font-size: 200%; /* Double la taille du bonhomme */
+}
+   
     button.btn-login {
         margin-left: auto;
         margin-right: auto;
         display: block;
         margin-top: 10px;
+    }
+
+    .profile-info {
+        margin-top: 5px;
+        font-size: 14px;
+        text-align: right;
+    }
+
+    button.btn-back {
+        background-color: orange;
+        color: white;
+        border: none;
+        margin-right: 10px;
     }
 
     @media (max-width: 767px) {
@@ -70,5 +54,31 @@
             margin-top: 10px;
             margin-bottom: 10px;
         }
+
+        .profile-info {
+            text-align: center;
+            margin-top: 5px;
+        }
     }
+
+    .navbar-inner {
+        display: flex;
+        justify-content: space-between; /* Aligner les éléments au début et à la fin */
+        align-items: center; /* Aligner les éléments verticalement */
+    }
+
+    .nav {
+        width: 100%; /* Assure que la liste des éléments occupe toute la largeur */
+        display: flex; /* Affiche les éléments en ligne */
+        align-items: center; /* Aligner les éléments verticalement */
+    }
+
+    .profile-info {
+        margin-left: auto; /* Déplace l'élément à droite */
+    }
+
+    .btn-back {
+        margin-right: 10px; /* Ajoute un espace après le bouton "Back" */
+    }
+
 </style>
