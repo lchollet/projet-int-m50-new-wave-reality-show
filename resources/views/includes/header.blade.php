e128304b6bf1d381a7ca6d30740bc074152b8d4d
lchollet <leo.chollet.2000@gmail.com>
2024-04-23 15:45:39 +0200

    add better logo and header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,20 +1,24 @@
 <div class="navbar">
-    <a href="/"><img src="{{ asset('storage/images/logo.png') }}" alt="logo new wave" class="logo"></a>
     <ul class="nav">
         <li class="back-button <?php if ($_SERVER['REQUEST_URI'] === '/') { echo 'hidden'; } ?>">
-            <button type="button" class="btn btn-warning btn-back" id="back" onclick="window.history.back();">&lt;</button>
+            <button type="button" class="btn btn-warning btn-back" id="back" onclick="window.history.back();"><img src="{{ asset('storage/images/Arrow.png') }}" alt="back arrow" class="arrow"></button>
+        </li >
+        <li class="empty">
+        </li>
+        <li class="empty">
         </li>
         <li class="profile-info">
-            <?php if (isset($_SESSION['user'])) { ?>
+            <?php if (isset($_SESSION['name'])) { ?>
                 <div class="user-profile">
                     <img src="{{ asset('storage/images/avatar.png') }}" alt="profil" id="avatar">
-                    <p><?php echo $_SESSION['user']['name']; ?></p>
+                    <p><?php echo $_SESSION['name']; ?></p> <!-- Afficher le nom d'utilisateur -->
                 </div>
             <?php } else { ?>
                 <button type="button" class="btn btn-warning btn-login" onclick="window.location.href='/login'">Login</button>
             <?php } ?>
-        </li>
+        </li>        
     </ul>
+    <a href="/"><img src="{{ asset('storage/images/logo.png') }}" alt="logo new wave" class="logo"></a>
 </div>
 
 
@@ -22,20 +26,25 @@
 <style>
 .navbar {
     text-align: center;
+    margin-top: 3%;
     margin-left: 3%;
     margin-right: 3%;
 }
 
 .nav {
-    list-style: none;
     display: flex;
-    justify-content: space-between; /* Espace les éléments */
-    align-items: center; /* Centre verticalement les éléments */ 
+    justify-content: space-between;
+    padding: 0 3%; /* Ajout de padding pour conserver les marges latérales */
 }
 
-.nav li {
-    margin-right: 10px; /* Ajoute un espace entre les éléments */
+.back-button {
+    margin-right: auto; /* Force le bouton de retour à rester à gauche */
 }
+
+.profile-info {
+    margin-left: auto; /* Force le bouton de profil à rester à droite */
+}
+
 
 #avatar {
     width: 50px;
@@ -52,9 +61,13 @@ button.btn-login:hover {
 }
 
 .btn-back {
-    background-color: var(--orange-2);
-    color: white;
+    background-color: transparent;
+    color: transparent;
     border: none;
+}
+
+.logo {
+    width: 200px;
 }
 
 
